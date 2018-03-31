e1fde23028601372a75513a943bf9ddd3d899899
Milan Felix Šulc <rkfelix@gmail.com>
2018-03-31 13:10:24 +0200

    Feature: added example of ajax form

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -2,7 +2,9 @@
 
 namespace App\Presenters;
 
+use Nette\Application\UI\Form;
 use Nette\Utils\DateTime;
+use Nette\Utils\Validators;
 
 class HomepagePresenter extends BasePresenter
 {
@@ -15,6 +17,35 @@ class HomepagePresenter extends BasePresenter
 	public function handleReload(string $box): void
 	{
 		$this->redrawControl($box);
+	}
+
+	protected function createComponentUserForm(): Form
+	{
+		$form = new Form();
+
+		$form->addText('username', 'Username');
+
+		$form->addText('age', 'Your age?');
+
+		$form->addSubmit('send', 'OK');
+
+		$form->onValidate[] = function (Form $form) {
+			if (!Validators::isNumeric($form->values->age)) {
+				$form->addError(sprintf('Invalid age input "%s" given', $form->values->age));
+			}
+
+			if ($form->hasErrors()) {
+				$this->redrawControl('userFormOk'); // for cleanup
+				$this->redrawControl('userFormError');
+			}
+		};
+
+		$form->onSuccess[] = function () {
+			$this->redrawControl('userFormError'); // for cleanup
+			$this->redrawControl('userFormOk');
+		};
+
+		return $form;
 	}
 
 }
