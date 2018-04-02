51166f89c67b3b74b19c6032c92f08a57841da4e
Milan Felix Šulc <rkfelix@gmail.com>
2018-04-02 13:32:24 +0200

    App: update ajax form and design

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -4,7 +4,8 @@ namespace App\Presenters;
 
 use Nette\Application\UI\Form;
 use Nette\Utils\DateTime;
-use Nette\Utils\Validators;
+use Nette\Utils\Html;
+use Nette\Utils\Strings;
 
 class HomepagePresenter extends BasePresenter
 {
@@ -23,26 +24,41 @@ class HomepagePresenter extends BasePresenter
 	{
 		$form = new Form();
 
-		$form->addText('username', 'Username');
+		$form->addText('username', 'Username')
+			->setRequired('Username is mandatory')
+			->setAttribute('placeholder', 'Type your name Mr.?');
 
-		$form->addText('age', 'Your age?');
+		$form->addText('email', 'Email')
+			->setHtmlAttribute('placeholder', 'Type your e-mail')
+			->setOption('description', Html::el('span')->setHtml('Try to type <strong>cool@nette.org</strong> to see validation.'))
+			->setEmptyValue('@')
+			->addFilter(function ($email) {
+				return Strings::lower($email);
+			})
+			->addRule($form::REQUIRED, 'E-mail is mandatory')
+			->addRule($form::EMAIL, 'Given e-mail is not e-mail');
+
+		$form->addInteger('age', 'Your age?')
+			->setHtmlAttribute('Are you young?')
+			->setNullable();
 
 		$form->addSubmit('send', 'OK');
 
 		$form->onValidate[] = function (Form $form) {
-			if (!Validators::isNumeric($form->values->age)) {
-				$form->addError(sprintf('Invalid age input "%s" given', $form->values->age));
-			}
-
-			if ($form->hasErrors()) {
-				$this->redrawControl('userFormOk'); // for cleanup
-				$this->redrawControl('userFormError');
+			// Validate e-mail duplicities (against DB?)
+			if (Strings::endsWith($form->values->email, '@nette.org')) {
+				$form->addError(sprintf('E-mail "%s" is already picked', $form->values->email));
 			}
 		};
 
-		$form->onSuccess[] = function () {
-			$this->redrawControl('userFormError'); // for cleanup
+		$form->onSubmit[] = function () {
+			// This method in invoked always
+			$this->redrawControl('userFormError');
 			$this->redrawControl('userFormOk');
+		};
+
+		$form->onSuccess[] = function () {
+			// Some handling on success...
 		};
 
 		return $form;
